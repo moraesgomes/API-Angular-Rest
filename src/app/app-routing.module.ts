f5dfa67daf55a611ce1fa3118a62748472c844bd
haroldo gomes <moraesgomes@gmail.com>
2023-05-17 17:21:16 -0300

    rota edição , tela cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UsuarioComponent } from './componente/usuario/usuario/usuario.component';
+import { UsuarioAddComponent } from './componente/usuario/usuario-add/usuario-add/usuario-add.component';
 
 const routes: Routes = [
 
    {path:'home',component:HomeComponent},
    {path:'login',component:LoginComponent},
    {path:'',component:LoginComponent},
-   {path:'usuarioList', component:UsuarioComponent}
+   {path:'usuarioList', component:UsuarioComponent},
+   {path:'usuarioAdd', component:UsuarioAddComponent},
+   {path:'usuarioAdd/:id', component:UsuarioAddComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component'; /*Requisições Ajax*/
 import { HttpInterceptorModule } from './service/header-interceptor.service';
 import { UsuarioComponent } from './componente/usuario/usuario/usuario.component';
+import { UsuarioAddComponent } from './componente/usuario/usuario-add/usuario-add/usuario-add.component';
 
 
 
@@ -18,7 +19,8 @@ import { UsuarioComponent } from './componente/usuario/usuario/usuario.component
     AppComponent,
     HomeComponent,
     LoginComponent,
-    UsuarioComponent
+    UsuarioComponent,
+    UsuarioAddComponent
   ],
   imports: [
     BrowserModule,
